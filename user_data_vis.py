bab4681470e6e6f176f069660c972bc4d274e841
Max Highsmith <maxrhighsmith@gmail.com>
2017-12-03 09:42:18 -0600

    fixed cosine distance

--- user_data_vis.py
+++ user_data_vis.py
@@ -23,7 +23,7 @@ ax = fig.add_subplot(111);
 ax.set_title("Meme Priors");
 
 ax.scatter(np.linspace(1, memePriors.size, memePriors.size), memePriors);
-plt.show();
+#plt.show();
 
 
 #collaborative filtering
@@ -36,7 +36,7 @@ TrainData     = memeMatrix[:, 2:UserTrainers];
 print(TrainData.shape);
 TestData      = memeMatrix[:, UserTrainers:numOfUsers];
 
-
+print(TrainData[0:29,0:29]);
 #########################
 #########################
 #May have to change right now treating un observed as voted down
@@ -49,27 +49,15 @@ for i in range(0,tsa):
     for j in range(0,tsb):
         if(np.isnan(TrainData[i,j])):
            TrainData[i,j]=0;
+print(TrainData[0:25,0:25]);
+similMat = np.full((tsa,tsa),-9);
+for i in range(0,tsa-2):
+    for j in range(0,tsa-2):
+        similMat[i,j] = cosine(TrainData[i,:], TrainData[j,:]);
+        similMat[i,j] = 1 - similMat[i,j];
+        #similMat[i,j] = 1- cosine(TrainData[i,:], TrainData[j,:]);
 
-print(TrainData);
-for i in range(0,numCM-2):
-    for j in range(0, numCM-1):
-        holder = cosine(TrainData[i,:], TrainData[j,:]);
-        if((math.isnan(holder))==False):
-            itemSimil[i,j] = 1-holder;  
-        else:
-            itemSimil[i,j] =-9;
-            print(i);
-            print(TrainData[i,1:]);
-            print(j);
-            print(TrainData[j,1:]);
-
-print(TrainData[0,1:]);
-print(1-cosine(TrainData[0,:],TrainData[0,:]));
-print(TrainData[1,1:]);
-print(1-cosine(TrainData[1,:],TrainData[1,:]));
-print(TrainData[2,1:]);
-print(1-cosine(TrainData[2,:],TrainData[2,:]));
-print(TrainData[3,1:]);
-print(1-cosine(TrainData[3,:],TrainData[3,:]));
-
-print(TrainData[0:20,0:20]);
+print(similMat[0:25,0:25]);
+print(TrainData[1,:]);
+print(TrainData[2,:]);
+print(cosine(TrainData[1,:], TrainData[2,:]));
